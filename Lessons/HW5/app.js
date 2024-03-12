e9952be4e61dae72d68992c0cdcae21234b54565
RaikhelsonV <raikhelsonvalerie@gmail.com>
2024-03-12 21:28:45 +0200

    hw5 for check

---
+++ Lessons/HW5/app.js
@@ -0,0 +1,13 @@
+import color from './color.js';
+import fruit from './fruit.js';
+import logger from './lib/logger/logger.js';
+import { add } from './math.js';
+
+const log = logger.getLogger('app.js');
+log.info(color);
+log.warn(fruit);
+log.error('occur: My log');
+
+log.debug('Some debug data');
+log.trace('Some trace data', 'Data', 'User');
+add(2, 5);
